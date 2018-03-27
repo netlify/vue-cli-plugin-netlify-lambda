b26fb25f88b46a0d24c106afad363f59ca5a38fd
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2018-03-27 10:03:40 -0500

    Add lambda to .gitignore

--- generator.js
+++ generator.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+
 module.exports = (api, options, rootOptions) => {
   api.render("./template")
+
+  fs.appendFileSync(api.resolve(".gitignore"), "\n# Compiled Lambda functions\n/lambda/\n")
 }
